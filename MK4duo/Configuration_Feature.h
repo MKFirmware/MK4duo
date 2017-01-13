ef9ec7da14a6153e7fd9b050c553e2aa415471f4
MagoKimbra <magokimbra@hotmail.com>
2017-01-13 11:53:54 +0100

    Fix encoder

--- MK4duo/Configuration_Feature.h
+++ MK4duo/Configuration_Feature.h
@@ -1110,7 +1110,6 @@
 //#define INVERT_BACK_BUTTON            // Option for invert back button logic if avaible
 
 // Encoder Direction Options
-
 // Test your encoder's behavior first with both options disabled.
 //
 //  Reversed Value Edit and Menu Nav? Enable REVERSE_ENCODER_DIRECTION.
@@ -1125,6 +1124,12 @@
 //  If CLOCKWISE normally moves DOWN this makes it go UP.
 //  If CLOCKWISE normally moves UP this makes it go DOWN.
 //#define REVERSE_MENU_DIRECTION
+
+#define ENCODER_RATE_MULTIPLIER         // If defined, certain menu edit operations automatically multiply the steps when the encoder is moved quickly
+#define ENCODER_10X_STEPS_PER_SEC 75    // If the encoder steps per sec exceeds this value, multiply steps moved x10 to quickly advance the value
+#define ENCODER_100X_STEPS_PER_SEC 160  // If the encoder steps per sec exceeds this value, multiply steps moved x100 to really quickly advance the value
+
+#define ULTIPANEL_FEEDMULTIPLY          // Comment to disable setting feedrate multiplier via encoder
 
 // SPEAKER/BUZZER
 // If you have a speaker that can produce tones, enable it here.
