fa3d347be02c6b3c6b4f5b9609753fa1cf795059
MagoKimbra <magokimbra@hotmail.com>
2017-04-18 09:42:39 +0200

    Fix Serial_m

--- MK4duo/src/MK_Main.cpp
+++ MK4duo/src/MK_Main.cpp
@@ -6156,7 +6156,7 @@ inline void gcode_G28() {
           char mess[15] = "No convergence";
           if (iterations < 31)
             sprintf_P(mess, PSTR("Iteration : %02i"), (int)iterations);
-          SERIAL_M(mess);
+          SERIAL_T(mess);
           SERIAL_M("                                   std dev:");
           SERIAL_V(zero_std_dev, 3);
           SERIAL_E;
