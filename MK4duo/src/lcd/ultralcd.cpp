43ff78fb4bb7f3a7519e9844552ef6030dbbc9c8
MagoKimbra <magokimbra@hotmail.com>
2017-11-03 13:55:35 +0100

    Fix broken lcd Case light

--- MK4duo/src/lcd/ultralcd.cpp
+++ MK4duo/src/lcd/ultralcd.cpp
@@ -759,7 +759,7 @@ void kill_screen(const char* lcd_msg) {
       //
       MENU_BACK(MSG_MAIN);
       MENU_ITEM_EDIT_CALLBACK(int8, MSG_CASE_LIGHT_BRIGHTNESS, &case_light_brightness, 0, 255, update_case_light, true);
-      MENU_ITEM_EDIT_CALLBACK(bool, MSG_CASE_LIGHT, (bool*)&case_light_brightness, update_case_light);
+      MENU_ITEM_EDIT_CALLBACK(bool, MSG_CASE_LIGHT, (bool*)&case_light_on, update_case_light);
       END_MENU();
     }
   #endif // HAS_CASE_LIGHT
