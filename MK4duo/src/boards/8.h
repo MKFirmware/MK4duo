f1a1eec05128811d6dd6e4f1457a946b7698edb7
Simone Persiani <iosonopersia@gmail.com>
2018-06-22 15:18:41 +0200

    Fix 8.h TEENSYLU

--- MK4duo/src/boards/8.h
+++ MK4duo/src/boards/8.h
@@ -1,9 +1,8 @@
 /****************************************************************************************
-* 8 - 81
-* Teensylu 0.7 / Printrboard pin assignments (AT90USB1286)
+* 8
+* Teensylu 0.7 pin assignments (AT90USB1286)
 * Requires the Teensyduino software with Teensy++ 2.0 selected in Arduino IDE!
-  http://www.pjrc.com/teensy/teensyduino.html
-* See http://reprap.org/wiki/Printrboard for more info
+* http://www.pjrc.com/teensy/teensyduino.html
 ****************************************************************************************/
 
 //###CHIP
@@ -114,9 +113,9 @@
 #define ORIG_Z_PROBE_PIN           NoPin
 
 //###SINGLE_ENDSTOP
-#define X_STOP_PIN                 NoPin
-#define Y_STOP_PIN                 NoPin
-#define Z_STOP_PIN                 NoPin
+#define X_STOP_PIN                 13
+#define Y_STOP_PIN                 14
+#define Z_STOP_PIN                 15
 
 //###HEATER
 #define ORIG_HEATER_0_PIN          21
@@ -128,11 +127,11 @@
 #define ORIG_COOLER_PIN            NoPin
 
 //###TEMPERATURE
-#define ORIG_TEMP_0_PIN            NoPin
+#define ORIG_TEMP_0_PIN             7
 #define ORIG_TEMP_1_PIN            NoPin
 #define ORIG_TEMP_2_PIN            NoPin
 #define ORIG_TEMP_3_PIN            NoPin
-#define ORIG_TEMP_BED_PIN          NoPin
+#define ORIG_TEMP_BED_PIN           6
 #define ORIG_TEMP_CHAMBER_PIN      NoPin
 #define ORIG_TEMP_COOLER_PIN       NoPin
 
@@ -163,25 +162,6 @@
 #define ORIG_LASER_PWR_PIN         NoPin
 #define ORIG_LASER_PWM_PIN         NoPin
 
-
 //###UNKNOWN_PINS
 #define AT90USB 1286  // Disable MarlinSerial etc.
-//@@@
-
-//###IF_BLOCKS
-// You may need to change ORIG_FAN0_PIN to 16 because Marlin isn't using fastio.h
-// for the fan and Teensyduino uses a different pin mapping.
-#if MB(TEENSYLU)  // Teensylu
-  #define X_STOP_PIN         13
-  #define Y_STOP_PIN         14
-  #define Z_STOP_PIN         15
-  #define ORIG_TEMP_0_PIN          7  // Extruder / Analog pin numbering
-  #define ORIG_TEMP_BED_PIN        6  // Bed / Analog pin numbering
-#else  // Printrboard
-  #define X_STOP_PIN         35
-  #define Y_STOP_PIN          8
-  #define Z_STOP_PIN         36
-  #define ORIG_TEMP_0_PIN          1  // Extruder / Analog pin numbering
-  #define ORIG_TEMP_BED_PIN        0  // Bed / Analog pin numbering
-#endif
 //@@@
