799b3ee16a05753827a673e19d437e5d26bbc0b5
MagoKimbra <magokimbra@hotmail.com>
2018-09-04 21:04:28 +0200

    Fix #579
    Fix read temp with MAX31855.

--- MK4duo/Configuration_Feature.h
+++ MK4duo/Configuration_Feature.h
@@ -747,12 +747,12 @@
  *****************************************************************************************/
 //#define DUAL_X_CARRIAGE
 
-#define X1_MIN_POS X_MIN_POS  // set minimum to ensure first x-carriage doesn't hit the parked second X-carriage
-#define X1_MAX_POS X_BED_SIZE // set maximum to ensure first x-carriage doesn't hit the parked second X-carriage
-#define X2_MIN_POS 80         // set minimum to ensure second x-carriage doesn't hit the parked first X-carriage
-#define X2_MAX_POS 353        // set maximum to the distance between toolheads when both heads are homed
-#define X2_HOME_DIR 1         // the second X-carriage always homes to the maximum endstop position
-#define X2_HOME_POS X2_MAX_POS // default home position is the maximum carriage position
+#define X1_MIN_POS X_MIN_POS    // set minimum to ensure first x-carriage doesn't hit the parked second X-carriage
+#define X1_MAX_POS X_BED_SIZE   // set maximum to ensure first x-carriage doesn't hit the parked second X-carriage
+#define X2_MIN_POS 80           // set minimum to ensure second x-carriage doesn't hit the parked first X-carriage
+#define X2_MAX_POS 353          // set maximum to the distance between toolheads when both heads are homed
+#define X2_HOME_DIR 1           // the second X-carriage always homes to the maximum endstop position
+#define X2_HOME_POS X2_MAX_POS  // default home position is the maximum carriage position
 // However: In this mode the HOTEND_OFFSET_X value for the second extruder provides a software
 // override for X2_HOME_POS. This also allow recalibration of the distance between the two endstops
 // without modifying the firmware (through the "M218 T1 X???" command).
