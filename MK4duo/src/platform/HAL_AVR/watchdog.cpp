69547d2683053d0f61a6d1e0c60372c2b4f9b15a
MagoKimbra <magokimbra@hotmail.com>
2018-11-09 13:51:21 +0100

    Clear code Watchdog AVR

--- MK4duo/src/platform/HAL_AVR/watchdog.cpp
+++ MK4duo/src/platform/HAL_AVR/watchdog.cpp
@@ -29,14 +29,24 @@
   // Initialize watchdog with a 4 sec interrupt time
   void Watchdog::init(void) {
     #if ENABLED(USE_WATCHDOG)
+      #if ENABLED(WATCHDOG_DURATION_8S) && ENABLED(WDTO_8S)
+        #define WDTO_NS WDTO_8S
+      #else
+        #define WDTO_NS WDTO_4S
+      #endif
       #if ENABLED(WATCHDOG_RESET_MANUAL)
         // We enable the watchdog timer, but only for the interrupt.
-        // Take care, as this requires the correct order of operation, with interrupts disabled. See the datasheet of any AVR chip for details.
+        // Take care, as this requires the correct order of operation, with interrupts disabled.
+        // See the datasheet of any AVR chip for details.
         wdt_reset();
+        cli();
         _WD_CONTROL_REG = _BV(_WD_CHANGE_BIT) | _BV(WDE);
-        _WD_CONTROL_REG = _BV(WDIE) | WDTO_4S;
+        _WD_CONTROL_REG = _BV(WDIE) | (WDTO_NS & 0x07) | ((WDTO_NS & 0x08) << 2); // WDTO_NS directly does not work. bit 0-2 are consecutive in the register but the highest value bit is at bit 5
+                                                                                  // So worked for up to WDTO_2S
+        sei();
+        wdt_reset();
       #else
-        wdt_enable(WDTO_4S);
+        wdt_enable(WDTO_NS); // The function handles the upper bit correct.
       #endif
     #endif // USE_WATCHDOG
   }
@@ -54,7 +64,8 @@
   // Watchdog timer interrupt, called if main program blocks >4sec and manual reset is enabled.
   #if ENABLED(USE_WATCHDOG) && ENABLED(WATCHDOG_RESET_MANUAL)
     ISR(WDT_vect) {
-      SERIAL_LM(ER, "Something is wrong, please turn off the printer.");
+      sei();  // With the interrupt driven serial we need to allow interrupts.
+      SERIAL_LM(ER, "Watchdog timeout. Reset required.");
       printer.kill(PSTR("ERR:Please Reset")); // kill blocks // 16 characters so it fits on a 16x2 display
       while (1); // wait for user or serial reset
     }
