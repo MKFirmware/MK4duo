365e93256e5432d9708a64c841a00bb6cb5718e7
MagoKimbra <magokimbra@hotmail.com>
2018-12-08 11:44:37 +0100

    Update revision 08122018

--- MK4duo/src/platform/platform.h
+++ MK4duo/src/platform/platform.h
@@ -35,6 +35,39 @@
 #include "common/communication.h"
 #include "common/servo.h"
 
+/**
+ * SPI speed where 0 <= index <= 6
+ *
+ * Approximate rates :
+ *
+ *  0 :  8 - 10 MHz
+ *  1 :  4 - 5 MHz
+ *  2 :  2 - 2.5 MHz
+ *  3 :  1 - 1.25 MHz
+ *  4 :  500 - 625 kHz
+ *  5 :  250 - 312 kHz
+ *  6 :  125 - 156 kHz
+ *
+ *  On AVR, actual speed is F_CPU/2^(1 + index).
+ *  On other platforms, speed should be in range given above where possible.
+ */
+
+#define SPI_FULL_SPEED      0   // Set SCK to max rate
+#define SPI_HALF_SPEED      1   // Set SCK rate to half of max rate
+#define SPI_QUARTER_SPEED   2   // Set SCK rate to quarter of max rate
+#define SPI_EIGHTH_SPEED    3   // Set SCK rate to 1/8 of max rate
+#define SPI_SIXTEENTH_SPEED 4   // Set SCK rate to 1/16 of max rate
+#define SPI_SPEED_5         5   // Set SCK rate to 1/32 of max rate
+#define SPI_SPEED_6         6   // Set SCK rate to 1/64 of max rate
+
+#define SPI_LSBFIRST 0
+#define SPI_MSBFIRST 1
+
+#define SPI_DATAMODE_0 0x00
+#define SPI_DATAMODE_1 0x04
+#define SPI_DATAMODE_2 0x08
+#define SPI_DATAMODE_3 0x0C
+
 #if ENABLED(ARDUINO_ARCH_SAM)
   #define CPU_32_BIT
   #include "HAL_DUE/spi_pins.h"
