6baccf2d65d12f116c1d7d2219bca8cef8dabe7f
MagoKimbra <magokimbra@hotmail.com>
2019-04-20 16:14:09 +0200

    Fix Nextion setText

--- MK4duo/src/lcd/nextionlcd/library/nextion.cpp
+++ MK4duo/src/lcd/nextionlcd/library/nextion.cpp
@@ -120,7 +120,7 @@ void NextionLCD::getText(NexObject &nexobject, char *buffer) {
 }
 
 void NextionLCD::setText(NexObject &nexobject, PGM_P buffer) {
-  char cmd[40];
+  char cmd[NEXTION_MAX_MESSAGE_LENGTH];
   sprintf_P(cmd, PSTR("p[%u].b[%u].txt=\"%s\""), nexobject.pid, nexobject.cid, buffer);
   sendCommand(cmd);
   recvRetCommandFinished();
